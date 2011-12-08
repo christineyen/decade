a53e6367f6a6bd004f8c41674dff4c7a891a76b4
Christine Yen <cyen@christineyen.com>
2011-12-08 02:10:47 -0800

    Adding/setting up application unit tests

---
+++ decadeApplicationTests/decadeApplicationTests.h
@@ -0,0 +1,13 @@
+//
+//  decadeApplicationTests.h
+//  decadeApplicationTests
+//
+//  Created by Christine Yen on 12/8/11.
+//  Copyright (c) 2011 MIT. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface decadeApplicationTests : SenTestCase
+
+@end
---
+++ decadeApplicationTests/decadeApplicationTests.m
@@ -0,0 +1,32 @@
+//
+//  decadeApplicationTests.m
+//  decadeApplicationTests
+//
+//  Created by Christine Yen on 12/8/11.
+//  Copyright (c) 2011 MIT. All rights reserved.
+//
+
+#import "decadeApplicationTests.h"
+
+@implementation decadeApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in decadeApplicationTests");
+}
+
+@end
